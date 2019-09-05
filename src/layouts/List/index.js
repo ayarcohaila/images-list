705c6b3f1d21a7ca3608511c67d323234c5710a6
James Sayer <jamesdsayer3@gmail.com>
2019-09-05 12:43:05 -0500

    feat: create Navbar and List layouts

---
+++ src/layouts/List/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+import { getImagesRequest } from '../../redux/images/actions';
+import { Item, Pagination } from '../../components';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 0,
+      pageLimit: 14,
+    };
+  }
+
+  componentDidMount() {
+    const { getImages } = this.props;
+    getImages();
+  }
+
+  handleChangePage = currentPage => {
+    this.setState({ currentPage });
+  };
+
+  render() {
+    const { list, loading } = this.props;
+    const { currentPage, pageLimit } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <Container>
+        <Row className="justify-content-center mb-4">
+          {list
+            .slice(currentPage * pageLimit, (currentPage + 1) * pageLimit)
+            .map(item => (
+              <Item key={item.id} data={item} />
+            ))}
+        </Row>
+        <Row className="justify-content-center">
+          <Pagination
+            currentPage={currentPage}
+            perPage={pageLimit}
+            total={list.length}
+            onChange={this.handleChangePage}
+          />
+        </Row>
+      </Container>
+    );
+  }
+}
+
+List.propTypes = {
+  list: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  list: state.images.list,
+  loading: state.images.loading,
+});
+
+const mapDispatchToProps = {
+  getImages: getImagesRequest,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(List);
